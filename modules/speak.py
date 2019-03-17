fe6793c16fa3281b45c34d2b41afc972d887d7be
Kelvin <kelvindsmn@gmail.com>
2019-03-17 11:20:12 +0700

    little add

--- modules/speak.py
+++ modules/speak.py
@@ -8,3 +8,7 @@ def speak(text):
         wrapper function to execute espeak command in shell via python
     """
     os.system('espeak -vid+f1 -k5 -s150 "{}"'.format(str(text)))
+
+
+if __name__ == "__main__":
+    speak("Hai Kelvin, apa kabar?")
