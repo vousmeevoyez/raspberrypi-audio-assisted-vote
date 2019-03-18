3c142cc1bd08135165bd54f4d62ff41bd5a5d249
Kelvin <kelvindsmn@gmail.com>
2019-03-19 05:59:10 +0700

    config

--- modules/listen.py
+++ modules/listen.py
@@ -196,8 +196,8 @@ def start():
         encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
         sample_rate_hertz=RATE,
         language_code=LANGUAGE,
-        speech_contexts=HELPER_KEYWORD,
-        #model=SPEECH_MODEL
+        speech_contexts=["masuk", "tampilkan", "pilih"],
+        model="command_and_search"
     )
     # initialize streaming config
     streaming_config = types.StreamingRecognitionConfig(
