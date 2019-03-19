753ee8277da38a160acc9a373259b2e1fdbcb609
Kelvin <kelvindsmn@gmail.com>
2019-03-19 07:04:34 +0700

    change ip

--- modules/config.py
+++ modules/config.py
@@ -9,7 +9,7 @@ LANGUAGE = "id-ID" # set indonesian language
 TIMEOUT = 10 # second time out
 HELPER_KEYWORD = ["pilih", "masuk", "tampilkan"]
 SPEECH_MODEL = "command_and_search"
-BASE_URL = "http://34.80.84.152:5000/api/v1"
+BASE_URL = "http://34.80.69.106:5000/api/v1"
 ROUTES = {
     "LOGIN"  : "/auth/login",
     "LOGOUT" : "/auth/logout",
