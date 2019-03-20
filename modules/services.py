6967e8e67173fbee1fe0493eace3e03b7c5aa21e
Kelvin <kelvindsmn@gmail.com>
2019-03-20 21:45:54 +0700

    test

--- modules/services.py
+++ modules/services.py
@@ -82,7 +82,8 @@ class VoteServices:
         sound_feedback = []
         candidates = []
         try:
-            response = self.remote_call(routes, None, election_id)
+            response = self.remote_call(routes=routes, identifier=election_id)
+            print(response)
         except ResponseError as error:
             if error.message == "ELECTION_NOT_FOUND":
                 message = "Pemilihan tidak ditemukan"
